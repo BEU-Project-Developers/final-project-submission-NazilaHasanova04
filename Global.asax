0fa6bb26f94e963a86f5fbf06c5a800a0ef4385a
Nazila Hasanova <159070592+NazilaHasanova04@users.noreply.github.com>
2024-06-20 19:52:36 +0400

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GroceryShopManagement.Global" Language="C#" %>
---
+++ Models/Functions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace GroceryShopManagement.Models
+{
+    public class Functions
+    {
+        private SqlConnection Con;
+        private SqlCommand cmd;
+        private DataTable tb;
+        private SqlDataAdapter adapter;
+        private string ConString;
+
+        public Functions()
+        {
+            ConString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Azay\Documents\GroceryDb.mdf;Integrated Security=True;Connect Timeout=30";
+            Con = new SqlConnection(ConString);
+            cmd = new SqlCommand();
+            cmd.Connection = Con;
+
+
+        }
+        public DataTable getData(string query)
+        {
+            tb = new DataTable();
+            adapter = new SqlDataAdapter(query, ConString);
+            adapter.Fill(tb);
+            return tb;
+        }
+        public int setData(string query)
+        {
+            int cnt = 0;
+            if (Con.State == ConnectionState.Closed)
+            {
+                Con.Open();
+            }
+            cmd.CommandText = query;
+            cnt = cmd.ExecuteNonQuery();
+            Con.Close();
+            return cnt;
+        }
+    }
+}
